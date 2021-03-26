7b240f0e85a8350cde0df3c43474e050cf5ea408
LucasCostakt <lucascosta.kt@gmail.com>
2021-03-26 17:50:24 -0300

    Adicionando func de consulta da api starwars
    A função adicionada vai consultar a api do starwars para ver a
    quantidade de vezes que o planeta inserido aparece nos filmes caso ele
    nunca tenha sempre retornará zero.

---
+++ service-planets/service/filmsCount.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type ArrayPlanet struct {
+	PP []PlanetFilmsCount `json:"results"`
+}
+
+type PlanetFilmsCount struct {
+	FilmURLs []string `json:"films"`
+}
+
+type httpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func NewRequest(method string, url string, requestBody []byte) (*http.Request, error) {
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, err
+	}
+	return request, err
+}
+
+const urlPlanets = "https://swapi.dev/api/planets/?search="
+
+func PlanetCount(planetName string) (int64, error) {
+
+	var countFilms int64
+
+	client := &http.Client{}
+	planetName = strings.ToLower(planetName)
+	url := urlPlanets + planetName
+	request, err := NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		return 0, fmt.Errorf("Erro no Request PlanetCount()")
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		return 0, fmt.Errorf("Erro no Response PlanetCount()")
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return 0, fmt.Errorf("Erro na leitura do Body PlanetCount()")
+	}
+
+	pl := &ArrayPlanet{}
+	json.Unmarshal(body, pl)
+
+	for _, b := range pl.PP {
+		countFilms = int64(len(b.FilmURLs))
+	}
+	return countFilms, nil
+}
