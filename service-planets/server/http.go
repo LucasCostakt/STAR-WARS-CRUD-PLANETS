f0432bae80c136980e0659570ef15113d838c62d
LucasCostakt <lucascosta.kt@gmail.com>
2021-03-28 23:16:17 -0300

    Adicionando metedos get, post e delete
    Adicionando metedos get, post e delete, com a integração com as funções
    do banco de dados

--- service-planets/server/http.go
+++ service-planets/server/http.go
@@ -1,10 +1,13 @@
 package server
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"service-consult/repository"
+	"strings"
 )
 
 type httpServer struct {
@@ -16,9 +19,13 @@ func NewServer() Storage {
 }
 
 func (h *httpServer) NewRoutes() {
-	log.Println("Init Routes")
+	log.Println("Iniciando Rotas")
+
+	client, _ := repository.NewMongoConnect()
+	serv := NewService(client)
+
 	router := http.NewServeMux()
-	router.Handle("/planets", http.HandlerFunc(planetsOperator))
+	router.Handle("/planets", http.HandlerFunc(serv.PlanetsOperator))
 
 	h.Handler = router
 }
@@ -30,23 +37,85 @@ func (h *httpServer) StartAPI() {
 		port = "5000"
 	}
 
-	log.Println("Start API")
-	log.Println("** Service Started on Port " + port + " **")
+	log.Println("API Iniciada")
+	log.Println("** Serviço inciado na Porta " + port + " **")
 	if err := http.ListenAndServe(":"+port, h); err != nil {
 		log.Fatal("init server error in StartApi(), ", err)
 	}
 }
 
-func planetsOperator(w http.ResponseWriter, r *http.Request) {
+func (serv *Server) PlanetsOperator(w http.ResponseWriter, r *http.Request) {
+
 	switch r.Method {
 	case http.MethodGet:
-		fmt.Fprintf(w, "Metodo get")
+		var response []byte
+		filters := &repository.Filters{}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Read request body error in PlanetsOperator(): ", err)
+		}
+		json.Unmarshal(body, filters)
+
+		if filters.Name != "" {
+			response, err = serv.Repository.CounsultPlanetByName(strings.ToLower(filters.Name))
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+		} else if filters.Id != "" {
+			response, err = serv.Repository.CounsultPlanetByID(filters.Id)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+		} else {
+			response, err = serv.Repository.CounsultAllPlanets()
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+		}
+		w.Write(response)
+
 	case http.MethodPost:
-		fmt.Fprintf(w, "Metodo post")
-	case http.MethodPut:
-		fmt.Fprintf(w, "Metodo put")
+		newInsertPlanet := repository.NewInsertPlanet{}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Erro ao ler request PlanetsOperator: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		json.Unmarshal(body, &newInsertPlanet)
+		countPlanet, err := PlanetCount(newInsertPlanet.Nome)
+		if err != nil {
+			log.Println("Erro ao ler request PlanetsOperator: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newInsertPlanet.Nome = strings.ToLower(newInsertPlanet.Nome)
+		newInsertPlanet.FilmsCount = countPlanet
+		response, err := serv.Repository.InsertNewPlanet(newInsertPlanet)
+		w.Write(response)
+
 	case http.MethodDelete:
-		fmt.Fprintf(w, "Metodo delete")
+		filters := &repository.Filters{}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Erro ao ler request PlanetsOperator: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		json.Unmarshal(body, filters)
+		err = serv.Repository.DeletePlanetById(filters.Id)
+		if err != nil {
+			log.Println("Erro DeletePlanetById() PlanetsOperator: ", err)
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+		w.Write([]byte("Documento deletado com Sucesso!"))
 	default:
 		w.WriteHeader(http.StatusNotFound)
 	}
